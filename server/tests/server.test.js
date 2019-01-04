2ccd739e4291aabb349ba96e61e1fa5443cba70d
Rodrigo Vieira <rodrigovieira@MacBook-Pro-de-Rodrigo.local>
2019-01-04 11:46:31 -0300

    Add Testing POST /users/login

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -214,11 +214,13 @@ describe('POST /users', () => {
           return done(err);
         }
 
-        User.findOne({ email }).then((userReturned) => {
-          expect(userReturned).toExist();
-          expect(userReturned.password).toNotBe(password);
-          done();
-        });
+        User.findOne({ email })
+          .then((userReturned) => {
+            expect(userReturned).toExist();
+            expect(userReturned.password).toNotBe(password);
+            done();
+          })
+          .catch(e => done(e));
       });
   });
 
@@ -235,6 +237,47 @@ describe('POST /users', () => {
       .post('/users')
       .send({ email: users[0].email, password: 'dummypass' })
       .expect(400)
+      .end(done);
+  });
+});
+
+describe('POST /users/login', () => {
+  it('should login user and return auth token', (done) => {
+    request(app)
+      .post('/users/login')
+      .send({
+        email: users[1].email,
+        password: users[1].password
+      })
+      .expect(200)
+      .expect((res) => {
+        expect(res.headers['x-auth']).toExist();
+      })
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+
+        User.findById(users[1]._id)
+          .then((user) => {
+            expect(user.tokens[0]).toInclude({
+              access: 'auth',
+              token: res.headers['x-auth'],
+            });
+            done();
+          })
+          .catch(e => done(e));
+      });
+  });
+
+  it('should reject invalid login', (done) => {
+    request(app)
+      .post('/users/login')
+      .send({ email: users[0].email, password: 'dummypass' })
+      .expect(400)
+      .expect((res) => {
+        expect(res.body).toEqual({});
+      })
       .end(done);
   });
 });
