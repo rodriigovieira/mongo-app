05b4097be4b76c04122fe92afe04536093ce3e21
Rodrigo Vieira <rodriigovieira@gmail.com>
2019-01-07 08:40:59 -0300

    removing comments

--- server/config/config.js
+++ server/config/config.js
@@ -8,12 +8,3 @@ if (env === 'development' || env === 'test') {
     process.env[value] = envConfig[value];
   });
 }
-
-
-// if (env === 'development') {
-//   process.env.PORT = 3000;
-//   process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
-// } else if (env === 'test') {
-//   process.env.PORT = 3000;
-//   process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
-// }
