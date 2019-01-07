421d2174e6b2f8248c726c8400e2c3aadd22243c
Rodrigo Vieira <rodriigovieira@gmail.com>
2019-01-07 08:26:36 -0300

    Fixing config

--- server/config/config.js
+++ server/config/config.js
@@ -1,7 +1,7 @@
 const env = process.env.NODE_ENV || 'development';
-const config = require('./config.json');
 
 if (env === 'development' || env === 'test') {
+  const config = require('./config.json');
   const envConfig = config[env];
 
   Object.keys(envConfig).forEach((value) => {
